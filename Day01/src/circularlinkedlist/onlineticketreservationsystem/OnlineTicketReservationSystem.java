a230a8af3d9224a87b4f55a5201ca46668225fee
omnema007 <nemaom2003@gmail.com>
2025-01-30 13:36:19 +0530

    I am done with online ticket reservation system

--- Day01/src/circularlinkedlist/onlineticketreservationsystem/OnlineTicketReservationSystem.java
+++ Day01/src/circularlinkedlist/onlineticketreservationsystem/OnlineTicketReservationSystem.java
@@ -3,7 +3,7 @@ package circularlinkedlist.onlineticketreservationsystem;
 public class OnlineTicketReservationSystem {
     public static void main(String[] args) {
         TicketReservationSystem system = new TicketReservationSystem();
-        
+
         system.addTicket(101, "Ravi", "Avatar 2", "A12", "2025-01-28 14:00");
         system.addTicket(102, "Deepak", "Spider-Man", "B3", "2025-01-28 15:00");
         system.addTicket(103, "Amit", "Avatar 2", "C5", "2025-01-28 16:00");
